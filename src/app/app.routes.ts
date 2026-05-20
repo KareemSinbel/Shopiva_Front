363d21c7e6dd9372d1daa896752f19586dcafe29
Mohamed Ellakany <136765196+Mohamed-Ellakany@users.noreply.github.com>
2026-05-20 04:04:35 +0300

    Make Structure of Auth Components edit styling structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './features/auth/components/login/login';
+import { Register } from './features/auth/components/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"login",component:Login , pathMatch:"full"},
+  {path:"register",component:Register , pathMatch:"full"},
+
+];

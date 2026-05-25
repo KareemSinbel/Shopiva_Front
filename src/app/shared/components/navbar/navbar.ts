ba9fbd412bdc75f4e1c56a686465cf270b02a9bd
ITI-Student <kareem.sinbel@gmail.com>
2026-05-25 05:07:05 +0300

    Authguard url parameter added| Navbar dynamic

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { AuthService } from '../../../core/services/auth-service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  isDropdownOpen = false;
+
+  get isLoggedIn(): boolean {
+    return this.auth.isLoggedIn();
+  }
+
+  get user() {
+    return this.auth.currentUser.value;
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  logout() {
+    this.isDropdownOpen = false;
+    this.auth.logout();
+    this.router.navigate(['/home']);
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('#user-menu-wrapper')) {
+      this.isDropdownOpen = false;
+    }
+  }
+}

07d335226230e0475eb38a83a7a7ec6a0ff42602
Kareem <74905162+KareemSinbel@users.noreply.github.com>
2026-06-01 12:24:59 +0300

    Comment out admin route configuration
    Commented out the admin route configuration in app.routes.ts.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,19 @@ import { MainLayout } from './layout/main-layout/main-layout';
 import { Dashboard } from './features/dashboard/pages/dashboard/dashboard';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: MainLayout,
-    children: [
-      {
-        path: 'dashboard',
-        component: Dashboard
-      },
-      {
-        path: '',
-        redirectTo: 'dashboard',
-        pathMatch: 'full'
-      }
-    ]
-  }
+  // {
+  //   path: 'admin',
+  //   component: AdminLayout,
+  //   children: [
+  //     {
+  //       path: 'dashboard',
+  //       component: Dashboard
+  //     },
+  //     {
+  //       path: '',
+  //       redirectTo: 'dashboard',
+  //       pathMatch: 'full'
+  //     }
+  //   ]
+  // },
 ];

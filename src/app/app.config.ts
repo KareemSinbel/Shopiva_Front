04139042a6449ec4893eef671c9491e82bb65b79
ITI-Student <kareem.sinbel@gmail.com>
2026-06-01 13:21:14 +0300

    resolve an isse with httpclientprovider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
@@ -10,8 +9,8 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideHttpClient(
-      withInterceptors([authInterceptor],
-      withFetch())
+      withInterceptors([authInterceptor]),
+      withFetch()
     )
 
   ]
